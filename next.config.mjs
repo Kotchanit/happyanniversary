d988771cff5ad6ad4204b6b0b850a755623a7b00
kotchanit.e <kotchanit.e@gmail.com>
2025-01-31 11:25:20 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"; // Detect production
 
 const nextConfig = {
-  output: "export",
-//   basePath: "/happyanniversary",
-//   assetPrefix: "/happyanniversary",
+  output: "export", // Required for GitHub Pages
+  basePath: isProd ? "/happyanniversary" : "", // Ensures correct paths in GitHub Pages
+  assetPrefix: isProd ? "/happyanniversary/" : "", // Fixes asset paths
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
   },
-  trailingSlash: true,
+  trailingSlash: true, // Ensures correct static file references
 };
 
 export default nextConfig;
